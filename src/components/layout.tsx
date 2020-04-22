e2cd26219dc0d43ad7095978a170f89528776539
Emil Johansson <emijohansson@deloitte.com>
2020-04-22 08:12:17 -0600

    moved custom global styles to layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import { createGlobalStyle } from 'styled-components'
 import './layout.scss'
 
 interface Props {
   children: any
 }
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper,
+  main {
+    height: 100%;
+  }
+`
 
 const Layout = ({ children }: Props) => (
   <StaticQuery
@@ -19,6 +30,7 @@ const Layout = ({ children }: Props) => (
     `}
     render={() => (
       <>
+        <GlobalStyle />
         {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
         <main>{children}</main>
         <footer></footer>
