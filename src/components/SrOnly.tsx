c5249a0639466da0302536f8b66c503e74df021a
Emil Johansson <emijohansson@deloitte.com>
2020-04-26 07:58:20 -0600

    add SrOnly component

---
+++ src/components/SrOnly.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  border: 0;
+  clip: rect(0, 0, 0, 0);
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+  height: 1px;
+`
