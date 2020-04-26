f9cea359c10f60e04e8b47bfd21489b1dfb12287
Emil Johansson <emijohansson@deloitte.com>
2020-04-26 08:48:57 -0600

    remove bootstrap and scss files

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,12 +8,49 @@ interface Props {
 }
 
 const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  html {
+    font-family: sans-serif;
+    line-height: 1.15;
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 300;
+    line-height: 1.5;
+    color: #212529;
+    text-align: left;
+    background-color: #fff;
+  }
   html,
   body,
   #___gatsby,
   #gatsby-focus-wrapper,
   main {
     height: 100%;
+  }
+
+  a {
+    background-color: transparent;
+    color: #282a36;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  a,
+  button,
+  input {
+    &:focus {
+      outline: -webkit-focus-ring-color auto 5px;
+    }
   }
 `
 
