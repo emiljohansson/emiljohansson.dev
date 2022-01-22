ca39aaae0a773bdd237158615240bc28e3ffad74
emiljohansson <emiljohansson.dev@gmail.com>
2022-01-22 13:26:51 -0700

    remove prettier and only use eslint with standard as base

---
+++ apps/next/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next',
+    '../../.eslintrc',
+  ],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+}
--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,22 +1,34 @@
 module.exports = {
-  extends: [
-    "next",
-    "prettier",
-    "plugin:@typescript-eslint/recommended"
-  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ['apps/*/', 'packages/*/'],
     },
   },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "import/no-anonymous-default-export": [0],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"]
+    'react/react-in-jsx-scope': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 }
