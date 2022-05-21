fb6e17353d9e3381e1e42da150aa64dce3d32612
emiljohansson <emiljohansson.dev@gmail.com>
2022-05-21 13:11:40 -0600

    add new app for password-manager

---
+++ apps/password-manager/next.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const withTM = require('next-transpile-modules')(['lib'])
+
+// module.exports = withTM({
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   experimental: {
+//     externalDir: true,
+//     runtime: 'nodejs',
+//     serverComponents: true,
+//   },
+// })
+module.exports = {
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+  },
+}
