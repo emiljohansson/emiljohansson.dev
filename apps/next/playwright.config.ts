bad7b903cbdd46e61d52868b22291eba8994a622
emiljohansson <emiljohansson.dev@gmail.com>
2022-05-28 08:17:01 -0600

    fix TRACING_ALREADY_STARTED issue in lighthouse tests

--- apps/next/playwright.config.ts
+++ apps/next/playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 10 * 1000,
+  timeout: 10 * 5000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
