abb764939e372de75b07494539ab94942cc730bd
emiljohansson <emiljohansson.dev@gmail.com>
2022-05-29 08:14:36 -0600

    restore eslint to remix

--- apps/remix-app/app/root.tsx
+++ apps/remix-app/app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -6,20 +6,20 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import styles from "./styles/app.css"
+} from '@remix-run/react'
+import styles from './styles/app.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
-export function links() {
-  return [{ rel: "stylesheet", href: styles }]
+export function links () {
+  return [{ rel: 'stylesheet', href: styles }]
 }
 
-export default function App() {
+export default function App () {
   return (
     <html lang="en">
       <head>
@@ -33,5 +33,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
