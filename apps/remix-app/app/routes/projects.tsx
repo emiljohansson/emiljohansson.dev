11f29b193c48cabad18db0ef878b5c5a9b4da561
emiljohansson <emiljohansson.dev@gmail.com>
2022-05-29 08:04:14 -0600

    restore tailwind into remix app

---
+++ apps/remix-app/app/routes/projects.tsx
@@ -0,0 +1,75 @@
+import type {
+  ActionFunction,
+  LoaderFunction,
+} from '@remix-run/node'
+import {
+  Form,
+  Link,
+  Outlet,
+  useLoaderData,
+  useTransition,
+} from '@remix-run/react'
+
+type Project = {
+  slug: string
+  title: string
+}
+
+let id = 1
+const projects: Project[] = [
+  {
+    slug: id.toString(),
+    title: 'test',
+  },
+]
+
+export const loader: LoaderFunction = async () => {
+  return projects
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  id++
+  const form = await request.formData()
+  const project = {
+    slug: id.toString(),
+    title: form.get('title')?.toString() || '',
+  }
+  projects.push(project)
+  console.log(projects)
+  return project
+}
+
+export default function Index () {
+  const projects = useLoaderData()
+  const { state } = useTransition()
+  const busy = state === 'submitting'
+
+  return (
+    <>
+      <div className="bg-slate-300 text-blue-400">
+        {projects.map(({ slug, title }: Project) => (
+          <Link key={slug} to={slug}>
+            {title}
+          </Link>
+        ))}
+        <Form method="post">
+          <input name="title" />
+          <button type="submit" disabled={busy}>
+            {busy ? 'Creating...' : 'Create New Project'}
+          </button>
+        </Form>
+      </div>
+      <div>OUTLET:</div>
+      <Link to="." prefetch="intent">
+        All
+      </Link>
+      <Link to="1" prefetch="intent">
+        first project
+      </Link>
+      <div>
+        <Outlet />
+      </div>
+      <div>end</div>
+    </>
+  )
+}
---
+++ apps/remix-app/app/routes/projects/$id.tsx
@@ -0,0 +1,46 @@
+import type { LoaderFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+type LoaderData = {
+  text: string
+  isOwner: boolean
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getUserId (request: Request) {
+  return 1
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function findUnique (query: { where: { id?: string } }) {
+  return {
+    id: 1,
+    text: 'abc item',
+  }
+}
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request)
+  const item = await findUnique({ where: { id: params.id } })
+
+  if (!item) {
+    throw new Response('What a joke! Not found.', { status: 404 })
+  }
+  const data: LoaderData = {
+    text: item.text,
+    isOwner: userId === item.id,
+  }
+
+  return data
+}
+
+export default function Invoice () {
+  const data = useLoaderData<LoaderData>()
+
+  return (
+    <div>
+      <div>text: {data.text}</div>
+      <div>isOwner: {data.isOwner.toString()}</div>
+    </div>
+  )
+}
