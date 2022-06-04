0bbb9242f30be26a61a497a065c902285668d5c7
emiljohansson <emiljohansson.dev@gmail.com>
2022-06-04 12:55:11 -0600

    add serverBuildTarget vercel to new remix app

--- apps/remix-new-app/remix.config.js
+++ apps/remix-new-app/remix.config.js
@@ -2,6 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
   ignoredRouteFiles: ["**/.*"],
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
 
