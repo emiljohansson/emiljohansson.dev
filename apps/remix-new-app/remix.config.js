749d8e45e4f9b5fa67a0895854475971adf26419
emiljohansson <emiljohansson.dev@gmail.com>
2022-06-04 12:30:13 -0600

    add server.js to new remix app

--- apps/remix-new-app/remix.config.js
+++ apps/remix-new-app/remix.config.js
@@ -3,6 +3,8 @@
  */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
