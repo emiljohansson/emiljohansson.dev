2b8b65b6f6e255002956e8dd326c13e04ad05e15
emiljohansson <emiljohansson.dev@gmail.com>
2022-06-11 12:30:04 -0600

    run prisma generate before build

--- apps/password-manager/lib/prisma.ts
+++ apps/password-manager/lib/prisma.ts
@@ -1,24 +1,9 @@
-// import { PrismaClient } from '@prisma/client'
-
-// let prisma: PrismaClient
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient()
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient()
-//   }
-//   prisma = global.prisma
-// }
-
-// export default prisma
-
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientPropertyName = '__prevent-name-collision__prisma'
 type GlobalThisWithPrismaClient = typeof globalThis & {
-  [prismaClientPropertyName]: PrismaClient;
-};
+  [prismaClientPropertyName]: PrismaClient
+}
 
 const getPrismaClient = () => {
   if (process.env.NODE_ENV === 'production') {
