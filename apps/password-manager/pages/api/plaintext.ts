f048dbd660f907d8d99895bae5e99efd98e54f0c
emiljohansson <emiljohansson.dev@gmail.com>
2022-06-11 09:13:09 -0600

    correct import syntax for cyrypto js

--- apps/password-manager/pages/api/plaintext.ts
+++ apps/password-manager/pages/api/plaintext.ts
@@ -1,15 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import * as crypto from 'crypto-js'
+import { AES, enc } from 'crypto-js'
 
 export default function handler (
   req: NextApiRequest,
   res: NextApiResponse<{ data: string }>,
 ) {
-  const bytes = crypto.AES.decrypt(req.body.password, req.body.secret)
+  const bytes = AES.decrypt(req.body.password, req.body.secret)
   try {
     res.status(200).json({
-      data: bytes.toString(crypto.enc.Utf8),
+      data: bytes.toString(enc.Utf8),
     })
   } catch (error) {
     res.status(200).json({
