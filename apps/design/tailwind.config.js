97bd6b6958023ecfdd4271a58677beef998d6652
emiljohansson <emiljohansson.dev@gmail.com>
2022-07-03 13:25:29 -0600

    add link and secondary button to design

--- apps/design/tailwind.config.js
+++ apps/design/tailwind.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const baseConfig = require('config/tailwind.config.js')
 
-baseConfig.plugins.push(require('@tailwindcss/typography'))
-
 module.exports = {
   ...baseConfig,
 }
