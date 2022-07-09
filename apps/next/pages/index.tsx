ab4af9fd2a698b1dcc6243cb2f7ad69cd5103d21
emiljohansson <emiljohansson.dev@gmail.com>
2022-07-09 15:36:06 -0600

    fix failing tests

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -19,52 +19,62 @@ const projects = [
     text: 'Design System',
     external: true,
     description: 'Design system for the apps developed by Emil Johansson.',
+    test: 'design-system',
   },
   {
     href: 'https://pw.emiljohansson.dev',
     text: 'Password Manager',
     external: true,
     description: 'Password manager for the apps developed by Emil Johansson.',
+    test: 'password-manager',
   },
   {
     href: '/random-string',
     text: 'Random String',
     description: 'Generate a random string of characters.',
+    test: 'random-string',
   },
   {
     href: '/current-time',
     text: 'Current Time',
     description: 'Get the current time.',
+    test: 'current-time',
   },
   {
     href: '/two-way-auth-generate',
     text: 'Two-Factor Authentication - Generate Code',
     description: 'Generate a two-factor authentication code.',
+    test: 'two-way-auth-generate',
   },
   {
     href: '/two-way-auth-enter',
     text: 'Two-Factor Authentication - Enter Code',
     description: 'Enter a two-factor authentication code.',
+    test: 'two-way-auth-enter',
   },
   {
     href: '/progress-bar',
     text: 'Progress Bar',
     description: 'Progress bar with a percentage.',
+    test: 'progress-bar',
   },
   {
     href: '/confirm-button',
     text: 'Confirm Button',
     description: 'Hold down the button to confirm the action.',
+    test: 'confirm-button',
   },
   {
     href: '/ms',
     text: 'Mine Sweaper',
     description: 'Mine sweaper game.',
+    test: 'mine-sweaper',
   },
   {
     href: '/calculate',
     text: 'Calculate',
     description: 'Calculate a mathematical expression from an API route.',
+    test: 'calculate',
   },
   // {
   //   href: '/rsc',
@@ -120,7 +130,7 @@ const HomePage = () => {
         </h1>
       </div>
       <div className="h-screen p-3 m-0 max-w-md mx-auto">
-        {projects.map(({ text, href, description, external }, index) => (
+        {projects.map(({ text, href, description, external, test }, index) => (
           <Link href={href} key={index}>
             <a
               className="
@@ -130,6 +140,7 @@ const HomePage = () => {
                 dark:bg-black-900 dark:shadow-lg-white
               "
               target={external ? '_blank' : undefined}
+              data-test={test}
             >
               <span className="flex items-center mb-1">{text} {
                 external && <ExternalLinkIcon width={18} height={18} className="absolute right-3" />
