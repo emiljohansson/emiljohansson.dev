96a7d571204e931b033d4549f074a6ea24b08686
emiljohansson <emiljohansson.dev@gmail.com>
2022-08-07 09:11:47 -0600

    add useEventListener

--- apps/next/pages/hooks.tsx
+++ apps/next/pages/hooks.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Layout from '@/components/Layout'
 import Header from '@/components/Header'
 import Content from '@/components/Content'
@@ -34,10 +34,26 @@ const useCounter = (initialValue = 0, options?: Partial<{
   return [count, increment, decrement, reset, set] as const
 }
 
+function useEventListener<T extends HTMLElement> (eventName: string, handler: (event: Event) => void) {
+  const ref = useRef<T>()
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener(eventName, handler)
+      return () => {
+        ref.current.removeEventListener(eventName, handler)
+      }
+    }
+  }, [eventName, handler])
+
+  return ref
+}
+
 const HooksPage = () => {
   const [value, setValue] = useState('')
   const [debounced] = useDebounceValue(value, 200)
   const [count, increment, decrement, reset, set] = useCounter(0, { min: 0, max: 10 })
+  const ref = useEventListener<HTMLButtonElement>('click', increment)
 
   return (
     <Layout>
@@ -49,14 +65,14 @@ const HooksPage = () => {
         <Header />
         <Section size="normal">
           <h1 className="sr-only">Hooks</h1>
-          <div className='flex flex-col gap-4'>
-            <div>
+          <article className='flex flex-col gap-4'>
+            <article>
               <h2>Debounce</h2>
               <input className="input" type="text" value={value} onChange={(e) => setValue(e.target.value)} />
               <p>Value: {value}</p>
               <p>Debounced: {debounced}</p>
-            </div>
-            <div>
+            </article>
+            <article>
               <h2>Counter</h2>
               <p>Count: {count}</p>
               <div className='flex'>
@@ -65,8 +81,12 @@ const HooksPage = () => {
                 <button className="btn-secondary" onClick={() => reset()}>Reset</button>
                 <button className="btn-secondary" onClick={() => set(5)}>Set 5</button>
               </div>
-            </div>
-          </div>
+            </article>
+            <article>
+              <h2>Event Listener</h2>
+              <button className="btn-primary" ref={ref}>Increment {count}</button>
+            </article>
+          </article>
         </Section>
       </Content>
     </Layout>
