9328dc3dec40e16c1f27a581e52e7ed77b10468f
emiljohansson <emiljohansson.dev@gmail.com>
2022-08-07 08:09:15 -0600

    add useCounter hook

--- apps/next/pages/hooks.tsx
+++ apps/next/pages/hooks.tsx
@@ -4,6 +4,7 @@ import Layout from '@/components/Layout'
 import Header from '@/components/Header'
 import Content from '@/components/Content'
 import Section from '@/components/Section'
+import { clamp } from 'lib/utils/number'
 
 const useDebounceValue = <T, >(value: T, delay = 300) => {
   const [debouncedValue, setDebouncedValue] = useState(value)
@@ -18,9 +19,25 @@ const useDebounceValue = <T, >(value: T, delay = 300) => {
   return [debouncedValue]
 }
 
+const useCounter = (initialValue = 0, options?: Partial<{
+  min: number
+  max: number
+}>) => {
+  const { min, max } = { min: -Infinity, max: Infinity, ...options }
+  const [count, setCount] = useState(clamp(initialValue, min, max))
+
+  const increment = () => setCount(clamp(count + 1, min, max))
+  const decrement = () => setCount(clamp(count - 1, min, max))
+  const reset = () => setCount(clamp(initialValue, min, max))
+  const set = (newValue: number) => setCount(clamp(newValue, min, max))
+
+  return [count, increment, decrement, reset, set] as const
+}
+
 const HooksPage = () => {
   const [value, setValue] = useState('')
   const [debounced] = useDebounceValue(value, 200)
+  const [count, increment, decrement, reset, set] = useCounter(0, { min: 0, max: 10 })
 
   return (
     <Layout>
@@ -30,11 +47,26 @@ const HooksPage = () => {
       </Head>
       <Content>
         <Header />
-        <Section size="large">
+        <Section size="normal">
           <h1 className="sr-only">Hooks</h1>
-          <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-          <p>Value: {value}</p>
-          <p>Debounced: {debounced}</p>
+          <div className='flex flex-col gap-4'>
+            <div>
+              <h2>Debounce</h2>
+              <input className="input" type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+              <p>Value: {value}</p>
+              <p>Debounced: {debounced}</p>
+            </div>
+            <div>
+              <h2>Counter</h2>
+              <p>Count: {count}</p>
+              <div className='flex'>
+                <button className="btn-secondary" onClick={() => increment()}>Increment</button>
+                <button className="btn-secondary" onClick={() => decrement()}>Decrement</button>
+                <button className="btn-secondary" onClick={() => reset()}>Reset</button>
+                <button className="btn-secondary" onClick={() => set(5)}>Set 5</button>
+              </div>
+            </div>
+          </div>
         </Section>
       </Content>
     </Layout>
