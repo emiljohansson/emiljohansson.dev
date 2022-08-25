58f28153ea65a955b7e209ee46d600459b51e507
Emil Johansson <emiljohansson.dev@gmail.com>
2022-08-25 06:50:24 +0200

    remove lib folder

--- apps/password-manager/pages/index.tsx
+++ apps/password-manager/pages/index.tsx
@@ -1,16 +1,9 @@
 import { Label } from '@radix-ui/react-label'
-import { Auth } from '@supabase/ui'
-import { useAuth } from 'lib/auth'
-import { supabase } from 'lib/client'
 
 export default function HomePage () {
-  const { user, view, session, signOut } = useAuth()
-  console.log({ user, session, signOut })
-
   return (
     <>
       <h1>Password Manager</h1>
-      {!user && <Auth view={view} supabaseClient={supabase} />}
       <form action="/accounts" method="get">
         <div className="mb-3">
           <Label htmlFor="secret" className="block pr-3">Secret</Label>
