db3eb8296d58864280468f7380b0c4d85d919c1c
Emil Johansson <emiljohansson.dev@gmail.com>
2022-09-04 09:51:57 +0200

    add lighthouse cypress task

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'cypress'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { lighthouse, prepareAudit } = require('@cypress-audit/lighthouse')
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents (on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        prepareAudit(launchOptions)
+      })
+
+      on('task', {
+        lighthouse: lighthouse(),
+      })
     },
   },
 })
