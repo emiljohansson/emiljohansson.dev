b5038b097a5dbb844d24c83eb056227d09d15149
Emil Johansson <emiljohansson.dev@gmail.com>
2022-09-06 11:17:03 +0200

    rename card-games to games

--- apps/games/lib/game.ts
+++ apps/games/lib/game.ts
@@ -1,4 +1,4 @@
-import { Card, Deck, Piles } from '@/types/card-games'
+import type { Card, Deck, Piles } from '@/types/card-games'
 
 export function scaleGame (gameEl: HTMLElement) {
   const visibleHeight = gameEl.offsetHeight
