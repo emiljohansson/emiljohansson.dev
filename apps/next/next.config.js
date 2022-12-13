8148528d98c8b3ab23325dcf3d11038718513f9d
emiljohansson <emiljohansson.dev@gmail.com>
2022-12-12 18:21:36 -0700

    include appDir to next config

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')(['lib'])
+// const withTM = require('next-transpile-modules')(['lib'])
 
-module.exports = withTM({
+// module.exports = withTM({
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
+    appDir: true,
     externalDir: true,
     // runtime: 'nodejs',
     // serverComponents: true,
   },
-})
+}
