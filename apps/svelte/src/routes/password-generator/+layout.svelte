5f0e697d63903e86cd382be0e289f1c90fea1399
emiljohansson <emiljohansson.dev@gmail.com>
2022-12-24 08:04:41 -0700

    fix left nav

--- apps/svelte/src/routes/password-generator/+layout.svelte
+++ apps/svelte/src/routes/password-generator/+layout.svelte
@@ -1,3 +1,20 @@
+<script type="ts">
+	const links = [
+		{
+			href: "/password-generator/random",
+			title: "Random",
+		},
+		{
+			href: "/password-generator/memorable",
+			title: "Memorable",
+		},
+		{
+			href: "/password-generator/pin",
+			title: "PIN",
+		},
+	]
+</script>
+
 <header class="bg-white border-b border-slate-200">
 	<div class="max-w-7xl mx-auto px-6 py-12">
 		<h1 class="text-4xl font-medium mb-0">Password Generator</h1>
@@ -7,24 +24,13 @@
 <section>
 	<div class="max-w-7xl mx-auto flex gap-6 px-6 py-12">
 		<nav class="grid w-[200px] md:w-[264px]">
-			<a
-				href="/password-generator/random"
-				class="flex items-center hover:bg-gray-200 rounded-md w-full px-3 leading-relaxed"
-			>
-				Random
-			</a>
-			<a
-				href="/password-generator/memorable"
-				class="flex items-center hover:bg-gray-200 rounded-md w-full px-3 leading-relaxed"
-			>
-				Memorable
-			</a>
-			<a
-				href="/password-generator/pin"
-				class="flex items-center hover:bg-gray-200 rounded-md w-full px-3 leading-relaxed"
-			>
-				PIN
-			</a>
+			<div>
+				{#each links as { href, title }}
+					<a {href} class="block hover:bg-gray-200 rounded-md w-full px-3 py-2 leading-6">
+						{title}
+					</a>
+				{/each}
+			</div>
 		</nav>
 
 		<main>
