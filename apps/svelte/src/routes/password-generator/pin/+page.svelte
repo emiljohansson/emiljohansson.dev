289ea779bbf963c95213c8919a7b0f621d4fd796
emiljohansson <emiljohansson.dev@gmail.com>
2022-12-24 08:21:12 -0700

    move randomString method to lib

--- apps/svelte/src/routes/password-generator/pin/+page.svelte
+++ apps/svelte/src/routes/password-generator/pin/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	const lettersPattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	const numericPattern = "0123456789"
-	const symbolsPattern = ",._?-:]&*#~}$>(<)@^|{%!+="
+	import { randomString } from "lib/utils/string"
 
 	const letters = false
 	const numeric = true
@@ -10,37 +8,6 @@
 	let value = randomString({ length, letters, numeric, symbols })
 
 	$: value = randomString({ length, letters, numeric, symbols })
-
-	function randomString(props?: {
-		length?: number
-		letters?: boolean
-		numeric?: boolean
-		symbols?: boolean
-	}) {
-		const { length, letters, numeric, symbols } = {
-			length: 10,
-			letters: true,
-			numeric: false,
-			symbols: false,
-			...props,
-		}
-		const pattern = [
-			letters && lettersPattern,
-			numeric && numericPattern,
-			symbols && symbolsPattern,
-		]
-			.filter((v) => !!v)
-			.join("")
-		let value = ""
-		let index = length
-
-		while (index--) {
-			const charIndex = Math.floor(Math.random() * pattern.length)
-			value += pattern[charIndex]
-		}
-
-		return value
-	}
 </script>
 
 <h2 class="text-2xl font-semibold mb-6">PIN</h2>
--- apps/svelte/src/routes/password-generator/random/+page.svelte
+++ apps/svelte/src/routes/password-generator/random/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	const lettersPattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	const numericPattern = "0123456789"
-	const symbolsPattern = ",._?-:]&*#~}$>(<)@^|{%!+="
+	import { randomString } from "lib/utils/string"
 
 	let value = randomString()
 	let length = 20
@@ -9,37 +7,6 @@
 	let symbols = false
 
 	$: value = randomString({ length, numeric, symbols })
-
-	function randomString(props?: {
-		length?: number
-		letters?: boolean
-		numeric?: boolean
-		symbols?: boolean
-	}) {
-		const { length, letters, numeric, symbols } = {
-			length: 10,
-			letters: true,
-			numeric: false,
-			symbols: false,
-			...props,
-		}
-		const pattern = [
-			letters && lettersPattern,
-			numeric && numericPattern,
-			symbols && symbolsPattern,
-		]
-			.filter((v) => !!v)
-			.join("")
-		let value = ""
-		let index = length
-
-		while (index--) {
-			const charIndex = Math.floor(Math.random() * pattern.length)
-			value += pattern[charIndex]
-		}
-
-		return value
-	}
 </script>
 
 <h2 class="text-2xl font-semibold mb-6">Random Password</h2>
