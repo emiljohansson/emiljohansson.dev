323371eb03dc30c90c97d35cb2817e6b3af3bdbf
emiljohansson <emiljohansson.dev@gmail.com>
2023-02-18 16:25:44 -0700

    add prettier

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,40 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  settings: {
-    next: {
-      rootDir: ['apps/*/', 'packages/*/'],
-    },
-  },
-  extends: [
-    'standard',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'import/no-anonymous-default-export': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-explicit-any': 'error',
-    'func-call-spacing': 'off',
-    '@typescript-eslint/func-call-spacing': 'error',
-    indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    'jsx-quotes': ['error', 'prefer-double'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		next: {
+			rootDir: ["apps/*/", "packages/*/"],
+		},
+	},
+	extends: ["standard", "plugin:@typescript-eslint/recommended", "prettier"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	// rules: {
+	// 	"react/react-in-jsx-scope": "off",
+	// 	"import/no-anonymous-default-export": "off",
+	// 	"comma-dangle": ["error", "always-multiline"],
+	// 	"no-unused-vars": "off",
+	// 	"@typescript-eslint/no-unused-vars": ["error"],
+	// 	"@typescript-eslint/no-explicit-any": "error",
+	// 	"func-call-spacing": "off",
+	// 	"@typescript-eslint/func-call-spacing": "error",
+	// 	"jsx-quotes": ["error", "prefer-double"],
+	// },
 }
