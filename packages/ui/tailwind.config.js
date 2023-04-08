071a8efe5ccf64a81546f629a73ccb93a0ad427e
Emil Johansson <emiljohansson.dev@gmail.com>
2023-04-08 10:46:42 -0600

    fix TW styles in ui package

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -3,6 +3,6 @@ const sharedConfig = require('config/tailwind.config.js')
 
 module.exports = {
 	// prefix ui lib classes to avoid conflicting with the app
-	prefix: 'ui-',
+	// prefix: 'ui-',
 	presets: [sharedConfig],
 }
