a3ca83bc468ffa51c7a5bfbe2d47c0691e3f22d4
Emil Johansson <emiljohansson.dev@gmail.com>
2023-05-08 07:12:12 -0600

    convert games to use inter from next/font

--- apps/games/src/app/layout.tsx
+++ apps/games/src/app/layout.tsx
@@ -3,6 +3,10 @@ import type { PropsWithChildren } from 'react'
 import 'ui/globals.css'
 import './globals.css'
 
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ weight: ['400', '500', '700'], subsets: ['latin'] })
+
 export const metadata = {
 	title: 'Games',
 	description: 'Games by Emil',
@@ -12,7 +16,7 @@ export default async function Layout({ children }: PropsWithChildren<unknown>) {
 	return (
 		<html lang="en">
 			<head></head>
-			<body>
+			<body className={inter.className}>
 				<main>{children}</main>
 			</body>
 		</html>
