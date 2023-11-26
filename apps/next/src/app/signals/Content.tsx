36c68861751029087de157350bc141e74426f437
Emil Johansson <emiljohansson.dev@gmail.com>
2023-11-26 12:21:56 -0700

    install preact signals and create a example page

---
+++ apps/next/src/app/signals/Content.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { signal, computed } from '@preact/signals-core'
+import './initSignals'
+
+const count = signal(0)
+const double = computed(() => count.value * 2)
+
+export default function Content() {
+	return (
+		<>
+			<h2>Counter</h2>
+			<p>Count: {count}</p>
+			<p>Double: {double}</p>
+			<div className="flex">
+				<button className="btn-secondary" onClick={() => count.value++}>
+					Increment
+				</button>
+				<button className="btn-secondary" onClick={() => count.value--}>
+					Decrement
+				</button>
+				<button
+					className="btn-secondary"
+					onClick={() => {
+						count.value = 0
+					}}
+				>
+					Reset
+				</button>
+				<button
+					className="btn-secondary"
+					onClick={() => {
+						count.value = 5
+					}}
+				>
+					Set 5
+				</button>
+			</div>
+		</>
+	)
+}
