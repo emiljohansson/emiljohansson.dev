48a673f89f160fabf8d96c6feaae26cfd42b74eb
Emil Johansson <emiljohansson.dev@gmail.com>
2024-01-01 09:23:44 +0100

    add page

---
+++ apps/pw2/app/add/AddAccount.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { useState } from 'react'
+
+const numbers = '0123456789'
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const r = (length: number, pattern: string) => {
+	let value = ''
+	let index = length
+	while (index--) {
+		const charIndex = Math.floor(Math.random() * pattern.length)
+		value += pattern[charIndex]
+	}
+	return value
+}
+
+const createPassword = () =>
+	`${r(3, characters)}-${r(3, characters)}${r(1, numbers)}${r(
+		3,
+		characters,
+	)}-${r(3, characters)}${r(1, numbers)}${r(3, characters)}`
+
+// function generate(secret: string) {
+// 	const actual = createPassword()
+// 	const encrypted = encrypt(actual, secret)
+
+// 	return {
+// 		actual,
+// 		encrypted,
+// 	}
+// }
+
+// function encrypt(value: string, secret: string, userId: string) {
+// 	const step1 = AES.encrypt(value, secret).toString()
+// 	const step2 = AES.encrypt(step1, userId).toString()
+// 	const step3 = AES.encrypt(
+// 		step2,
+// 		process.env.NEXT_PUBLIC_ENCODE_SECRET,
+// 	).toString()
+// 	return step3
+// }
+
+// function decrypt(password: string, secret: string, userId: string) {
+// 	const step3 = AES.decrypt(
+// 		password,
+// 		process.env.NEXT_PUBLIC_ENCODE_SECRET,
+// 	).toString(enc.Utf8)
+// 	const step2 = AES.decrypt(step3, userId).toString(enc.Utf8)
+// 	const step1 = AES.decrypt(step2, secret).toString(enc.Utf8)
+// 	console.log({
+// 		x: process.env.NEXT_PUBLIC_ENCODE_SECRET,
+// 		secret,
+// 		step1,
+// 		step2,
+// 		step3,
+// 	})
+
+// 	return step1
+// }
+
+export function AddAccount() {
+	const [actual, setActual] = useState(createPassword())
+	const [secret, setSecret] = useState('')
+
+	return (
+		<>
+			<input type="text" name="website" placeholder="Website" className="p-4" />
+			<input
+				type="text"
+				name="username"
+				placeholder="Username"
+				className="p-4"
+			/>
+			<input
+				type="text"
+				name="password"
+				className="p-4"
+				value={actual}
+				onChange={({ target }) => {
+					console.log(target.value)
+
+					setActual(target.value)
+				}}
+			/>
+			<input
+				type="text"
+				name="secret"
+				value={secret}
+				onChange={({ target }) => {
+					setSecret(target.value)
+				}}
+			/>
+			<button type="submit">Save</button>
+		</>
+	)
+}
---
+++ apps/pw2/app/add/page.tsx
@@ -0,0 +1,53 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { AES } from 'crypto-js'
+import { createClient } from '@/utils/supabase/server'
+import { AddAccount } from './AddAccount'
+
+async function savePassword(formData: FormData) {
+	'use server'
+	const cookieStore = cookies()
+	const supabase = createClient(cookieStore)
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+
+	console.log({ user })
+	if (!user) {
+		redirect('/login')
+	}
+
+	const secret = formData.get('secret') as string
+	const website = formData.get('website') as string
+	const username = formData.get('username') as string
+	const actualPassword = formData.get('password') as string
+
+	const step1 = AES.encrypt(actualPassword, secret).toString()
+	const step2 = AES.encrypt(step1, user.id).toString()
+	const password = AES.encrypt(step2, process.env.ENCRYPT_SECRET).toString()
+
+	console.log(password)
+	console.log({ secret, website, username, password })
+
+	await supabase.from('account').insert({
+		userId: user.id,
+		website,
+		username,
+		password,
+	})
+
+	return {
+		success: true,
+	}
+}
+
+export default function AddPage() {
+	return (
+		<>
+			<h1 className="heading1">Add account</h1>
+			<form action={savePassword} method="post">
+				<AddAccount />
+			</form>
+		</>
+	)
+}
