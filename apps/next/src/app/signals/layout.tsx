18a9f549111138cfd2cb81e06a3b4d41e29d75b2
Emil Johansson <emiljohansson.dev@gmail.com>
2024-12-23 10:53:01 +0100

    impl preact signals

---
+++ apps/next/src/app/signals/layout.tsx
@@ -0,0 +1,10 @@
+import type { PropsWithChildren } from 'react'
+
+export const metadata = {
+	title: 'Signals',
+	description: 'Preact Signals in Next.js',
+}
+
+export default function Layout({ children }: PropsWithChildren<unknown>) {
+	return children
+}
