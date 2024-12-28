a98663c416308d8bc42cb602d86d97faa09d2067
Emil Johansson <emiljohansson.dev@gmail.com>
2024-12-28 09:47:41 +0100

    use new tw config in games

---
+++ apps/games/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+import sharedConfig from '@repo/tailwind-config'
+
+const config = {
+	content: ['./src/app/**/*.tsx'],
+	presets: [sharedConfig],
+	safelist: ['bg-yellow-400', 'bg-gray-400', 'bg-green-400'],
+} satisfies Config
+
+export default config
