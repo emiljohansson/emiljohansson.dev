b0d54678ff924f64394adde27ade1b8d78cf8177
Emil Johansson <emiljohansson.dev@gmail.com>
2024-12-30 12:45:32 +0100

    replace current time hook with a global signal

--- apps/next/src/app/current-time/Card.tsx
+++ apps/next/src/app/current-time/Card.tsx
@@ -1,15 +1,13 @@
 'use client'
 
-import useCurrentTime from '@repo/lib/hooks/useCurrentTime'
+import { currentTime } from '@/lib/currentTime'
 
 export function Card() {
-	const { hours, minutes, meridiem } = useCurrentTime()
-
 	return (
 		<div className="flex items-center justify-center text-5xl h-full">
 			<div>
-				{hours}:{minutes}
-				<span className="text-3/6 meridiem -mb-5">{meridiem}</span>
+				{currentTime.hours}:{currentTime.minutes}
+				<span className="text-3/6 meridiem -mb-5">{currentTime.meridiem}</span>
 			</div>
 		</div>
 	)
--- apps/next/src/app/current-time/page.tsx
+++ apps/next/src/app/current-time/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import useCurrentTime from '@repo/lib/hooks/useCurrentTime'
+import { currentTime } from '@/lib/currentTime'
 
 const CurrentTimePage = () => {
-	const { hours, minutes, meridiem } = useCurrentTime()
-
 	return (
 		<>
-			{hours}:{minutes}
-			<span className="text-3/6 meridiem -mb-5">{meridiem}</span>
+			{currentTime.hours}:{currentTime.minutes}
+			<span className="text-3/6 meridiem -mb-5">{currentTime.meridiem}</span>
 		</>
 	)
 }
