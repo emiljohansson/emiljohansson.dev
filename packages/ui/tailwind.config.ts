a64484d5155bfdc82a70b333777072114bc79585
Emil Johansson <emiljohansson.dev@gmail.com>
2025-01-01 14:29:04 +0100

    add typography and shadcn button to ui package

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,12 +1,56 @@
 import type { Config } from 'tailwindcss'
 import sharedConfig from '@repo/tailwind-config'
 
-console.log('sharedConfig:', sharedConfig)
-
-const config: Pick<Config, 'prefix' | 'presets' | 'content'> = {
+const config = {
+	darkMode: ['class'],
 	content: ['./src/**/*.tsx'],
-	prefix: 'ui-',
 	presets: [sharedConfig],
-}
+	plugins: [require('tailwindcss-animate')],
+	prefix: 'ui-',
+	theme: {
+		extend: {
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			borderRadius: {
+				lg: `var(--radius)`,
+				md: `calc(var(--radius) - 2px)`,
+				sm: 'calc(var(--radius) - 4px)',
+			},
+		},
+	},
+} satisfies Config
 
 export default config
